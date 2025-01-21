25f837cf81c733441ea658f71d8eff67109c1fa1
Augusto Dantas <augustodantas.dev@gmail.com>
2025-01-21 15:44:26 -0300

    Feature: mongoDB conection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes';
+import mongoose from 'mongoose';
 
-const app = express();
+dotenv.config();
+
+const app = express();	
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Conectado ao MongoDB!'))
+.catch((error) =>console.error('Erro ao conectar ao MongoDB:', error));
 
 app.use(express.json());
 app.use(routes);
 
-export default app; 
+export default app;
