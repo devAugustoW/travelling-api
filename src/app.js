6b00da9bf69325ef399ff16929c29a4a6f0d97e8
Augusto Dantas <augustodantas.dev@gmail.com>
2025-01-24 16:03:23 -0300

    Feature: cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,15 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI)
 	.then(() => console.log('Conectado ao MongoDB!'))
 	.catch((error) =>console.error('Erro ao conectar ao MongoDB:', error));
+
+// Configuração do CORS
+app.use(cors({
+  origin: '*', 
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+	optionsSuccessStatus: 200 
+}));
 
 app.use(express.json());
 app.use(routes);
